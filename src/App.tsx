039ff928303ac1399333d82dc2eb62b252126cd9
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-18 15:37:59 -0500

    83. Diseño de pantalla y estilos globales

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import {StatusBar, View} from 'react-native';
 import {CalculatorScreen} from './presentation/screens/CalculatorScreen';
+import {styles} from './config/theme/app-theme';
 
 function App() {
   return (
-    <View>
+    <View style={styles.background}>
       <StatusBar barStyle={'light-content'} backgroundColor={'black'} />
       <CalculatorScreen />
     </View>
--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,9 +1,13 @@
 import {Text, View} from 'react-native';
+import {styles} from '../../config/theme/app-theme';
 
 export const CalculatorScreen = () => {
   return (
-    <View>
-      <Text>CalculatorScreen</Text>
+    <View style={styles.calculatorContainer}>
+      <View style={{paddingHorizontal: 30, paddingBottom: 20}}>
+        <Text style={styles.mainResult}>1500</Text>
+        <Text style={styles.subResult}>15</Text>
+      </View>
     </View>
   );
 };
