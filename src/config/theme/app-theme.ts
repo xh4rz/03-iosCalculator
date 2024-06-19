e0957333c7e08b8ab6d11aba870407b3aec3de07
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-06-18 19:30:16 -0500

    84. Botones y estilos

--- src/config/theme/app-theme.ts
+++ src/config/theme/app-theme.ts
@@ -32,4 +32,25 @@ export const styles = StyleSheet.create({
     textAlign: 'right',
     fontWeight: '300',
   },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 18,
+    paddingBottom: 10,
+  },
+  button: {
+    height: 80,
+    width: 80,
+    backgroundColor: colors.darkGray,
+    borderRadius: 100,
+    justifyContent: 'center',
+    marginHorizontal: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    color: 'white',
+    fontWeight: '300',
+  },
 });
--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,4 +1,4 @@
-import {Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import {styles} from '../../config/theme/app-theme';
 
 export const CalculatorScreen = () => {
@@ -7,6 +7,12 @@ export const CalculatorScreen = () => {
       <View style={{paddingHorizontal: 30, paddingBottom: 20}}>
         <Text style={styles.mainResult}>1500</Text>
         <Text style={styles.subResult}>15</Text>
+      </View>
+
+      <View style={styles.row}>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>1</Text>
+        </Pressable>
       </View>
     </View>
   );
